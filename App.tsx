f9d411eb9dde0da5a890335ec4101890e667e317
PramisGrg <gurungpramis361@gmail.com>
2025-12-12 11:27:55 +0545

    feat: android and ios bundled

--- App.tsx
+++ App.tsx
@@ -5,16 +5,15 @@ import {
   useColorScheme,
   View,
 } from 'react-native';
-import { SafeAreaProvider } from 'react-native-safe-area-context';
 
 function App() {
   const isDarkMode = useColorScheme() === 'dark';
 
   return (
-    <SafeAreaProvider>
+    <>
       <StatusBar barStyle={isDarkMode ? 'light-content' : 'dark-content'} />
       <AppContent />
-    </SafeAreaProvider>
+    </>
   );
 }
 
